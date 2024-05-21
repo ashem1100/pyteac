39b95a601f49648044da18f8167d82cbef37ea20
ashem1100 <ashkanemami2005@gmail.com>
2024-05-21 23:44:28 +0330

    hi

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-index = input("Enter a text: ")
-x , opr , y = index.split(" ")
+def main():
+    index = input("Enter a time: ")
+    num = convert(index)
+    if 8.0 >= num >= 7.0:
+        print("breakfast time")
+    elif 13.0 >= num >= 12.0:
+        print("lunch time")
+    elif 19.0 >= num >= 18.0:
+        print("dinner time")
+    else:
+        return 0
 
-match opr:
-    case "+":
-      print(float(x) + float(y))
-    case "-":
-      print(float(x) - float(y))
-    case "*":
-      print(float(x) * float(y))
-    case "/":
-      print(float(x) / float(y))
+
+
+def convert(time):
+    h,m = time.split(":")
+    hour = int(h)
+    min = int(m)
+
+    result = float(hour+(min/60))
+
+    return result
+
+
+
+main()
